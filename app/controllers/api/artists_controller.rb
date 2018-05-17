e64b4fc4dcf003f5c333f1ea8350de8fb4ab248d
yungwifi <smerryman@me.com>
2018-05-17 11:54:14 -0400

    full crud on artists

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,6 +1,40 @@
 class Api::ArtistsController < ApplicationController
+
     def index 
-        @artist = Artist.all 
+        @artists = Artist.all 
+
+        render json: @artists
+    end
+
+    def show 
+        @artist = Artist.find(params[:id])
+
         render json: @artist
     end
+
+    def create 
+        @artist = Artist.create!(artist_params)
+
+        render json: @artist
+    end
+
+
+    def update 
+        @artist = Artist.find(params[:id])
+        @artist.update!(artist_params)
+
+        render json: @artist
+    end
+
+    def destroy
+        @artist = Artist.find(params[:id])
+        
+        @artist.destroy
+    end
+
+    private
+    def artist_params
+        params.require(:artist).permit(:name, :photo_url, :nationality)
+      end
+
 end
